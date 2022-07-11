1331c6089c1de5aded8f4d79d1c0026f617333dc
BilalU123 <uddinbilal@aol.com>
2022-07-11 09:00:16 +0200

    If

--- script_01.js
+++ script_01.js
@@ -2,3 +2,36 @@
 
 // Entscheidungsstrukturen | control flow
 
+//Deklaration + Assignment
+const ageJohn = 30;
+const ageMark = 30;
+
+//Deklaration
+let isJohnOlder, isJohnEqual;
+
+// Test | Logische Aussage
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+
+ //Ausgabe
+ console.log("ageJohn: " + ageJohn);
+ console.log("ageMark: " + ageMark);
+ console.log("isJohnOlder: " + isJohnOlder);
+ console.log("isJohnEqual: " + isJohnEqual);
+ console.log("------------------");
+
+ //*******************  IF   ****************** */
+// TINA ---> tHERE IS NO aLTERNATIVE 
+// entweder JA oder nix .... Alternativelos
+//if(true)
+//if(false)
+//if(isJohnOlder)
+if(ageJohn > ageMark)
+{
+     console.log("John is älter!");
+}
+
+//*******************  IF - ELSE   ****************** */
+// mit Alternative
+// entweder JA oder Nein
