d60393e66cd01932f2037b77d8ab5dd8c51e9dc9
BilalU123 <uddinbilal@aol.com>
2022-07-11 10:22:07 +0200

    04 /Switch / Case 1

--- script_01.js
+++ script_01.js
@@ -3,23 +3,23 @@
 // Entscheidungsstrukturen | control flow
 
 //Deklaration + Assignment
-const ageJohn = 35;
-const ageMark = 30;
+//const ageJohn = 35;
+//const ageMark = 30;
 
 //Deklaration
-let isJohnOlder, isJohnEqual;
+//let isJohnOlder, isJohnEqual;
 
 // Test | Logische Aussage
-isJohnOlder = (ageJohn > ageMark);
-isJohnEqual = (ageJohn == ageMark);
+//isJohnOlder = (ageJohn > ageMark);
+//isJohnEqual = (ageJohn == ageMark);
 
 
  //Ausgabe
- console.log("ageJohn: " + ageJohn);
- console.log("ageMark: " + ageMark);
- console.log("isJohnOlder: " + isJohnOlder);
- console.log("isJohnEqual: " + isJohnEqual);
- console.log("------------------");
+ //console.log("ageJohn: " + ageJohn);
+ //console.log("ageMark: " + ageMark);
+ //console.log("isJohnOlder: " + isJohnOlder);
+ //console.log("isJohnEqual: " + isJohnEqual);
+ //console.log("------------------");
 
  //*******************  IF   ****************** */
 // TINA ---> tHERE IS NO aLTERNATIVE 
@@ -51,17 +51,49 @@ isJohnEqual = (ageJohn == ageMark);
  /************  IF - ELSE - IF ************/
  //Mit alternativen Fällen (älter, jünger, gleich alt)
 
- if (isJohnOlder)                    // 1. variante
- {
-    console.log("John is älter!"); 
-}  
-else if (isJohnEqual)                 //2. Fall     
- {
-    console.log("John is gleich alt!"); 
-}                                    //evt. weitere alternative (n) else if(){}
-else                                // Gemeinsame else-zweig
-{
-    console.log("John is Jünger!"); 
-}
+ //if (isJohnOlder)                    // 1. variante
+ //{
+    //console.log("John is älter!"); 
+//}  
+//else if (isJohnEqual)                 //2. Fall     
+ //{
+    //console.log("John is gleich alt!"); 
+//}                                    //evt. weitere alternative (n) else if(){}
+//else                                // Gemeinsame else-zweig
+//{
+    //console.log("John is Jünger!"); 
+//}
 
-/************  Fallunterscheidung / SWITCH | CASE ************/
+
+/************  Fallunterscheidung / SWITCH | CASE 1 ************/
+
+ const firstName = "Jane";
+ let job;
+
+ job = "driver";     // .. fährt TAXI! / UBER
+ job = "diver";      // .. taucht im Rhein! 
+ job = "artist";     // .. malt ein Bild!
+ job = "pilot";      // .. macht etwas anderes! --> default
+ job = "teacher";    // .. unterrichtet!
+ job = "instructor";  // .. unterrichtet!
+
+switch (job) 
+{
+    case "driver":  //Test auf ==
+        console.log(firstName + "fährt TAXI!");
+        break;
+    case "diver":  //taucht im Rhein!
+        console.log(firstName + " taucht im Rhein!");
+        break;
+    case "artist":  //malt ein Bild!
+        console.log(firstName + " malt ein Bild!");
+        break;
+    case "teacher":  //unterrichtet!
+    case "instructor":  //unterrichtet!
+        console.log(firstName + " instructor !");
+        break;
+    
+        default:    //Wichtig
+        console.log(firstName + " macht etwas anderes!");
+        break;
+}
